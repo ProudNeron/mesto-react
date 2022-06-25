25b2aeb82cf3a580fdb8146724e3fac8d3f7fe9b
ProudNeron <neronnp4@gmail.com>
2022-06-25 13:11:49 +0700

    fixed popupwithform component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, {useState} from "react";
 import Header from "./Header.js";
 import Main from "./Main.js";
-import Footer from "./Footer";
+import Footer from "./Footer.js";
 import PopupWithForm from "./PopupWithForm.js";
-import ImagePopup from "./ImagePopup";
+import ImagePopup from "./ImagePopup.js";
 
 function App() {
-  const [isEditProfilePopupOpen, setEditProfilePopupState] = useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpenState] = useState(false);
   const [isAddPlacePopupOpen, setAddPlacePopupState] = useState(false);
   const [isEditAvatarPopupOpen, setEditAvatarPopupState] = useState(false);
   const [selectedCard, setSelectedCardState] = useState(null);
@@ -16,7 +16,7 @@ function App() {
   }
 
   function handleEditProfileClick() {
-    setEditProfilePopupState(true);
+    setIsEditProfilePopupOpenState(true);
   }
 
   function handleAddPlaceClick() {
@@ -29,7 +29,7 @@ function App() {
 
   function closeAllPopups() {
     setEditAvatarPopupState(false);
-    setEditProfilePopupState(false);
+    setIsEditProfilePopupOpenState(false);
     setAddPlacePopupState(false);
     setSelectedCardState(null);
   }
@@ -43,32 +43,29 @@ function App() {
         <Footer />
       </div>
       <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-      <PopupWithForm title={'Редактировать профиль'}
-                     name={'edit-user-data'} isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+      <PopupWithForm title='Редактировать профиль' name={'edit-user-data'}
+                     isOpen={isEditProfilePopupOpen} btnText={'Сохранить'} onClose={closeAllPopups}>
         <input type="text" required id="user-name" minLength="2" maxLength="40"
                className="popup__form-item popup__form-item_user_name text-input"/>
         <span className="popup__input-error user-name-error"></span>
         <input type="text" required id="user-about" minLength="2" maxLength="200"
                className="popup__form-item popup__form-item_user_about text-input"/>
         <span className="popup__input-error user-about-error"></span>
-        <button type="submit" aria-label="Сохранить" className="popup__submit-btn">Сохранить</button>
       </PopupWithForm>
-      <PopupWithForm title={'Новое место'}
-                     name={'add-card'} isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+      <PopupWithForm title='Новое место' name={'add-card'}
+                     isOpen={isAddPlacePopupOpen} btnText={'Создать'} onClose={closeAllPopups}>
         <input type="text" required id="place-name" minLength="2" maxLength="30" placeholder="Название"
                className="popup__form-item popup__form-item_image_name text-input"/>
         <span className="popup__input-error place-name-error"></span>
         <input type="url" required id="place-url" placeholder="Ссылка на картинку"
                className="popup__form-item popup__form-item_image_url text-input"/>
         <span className="popup__input-error place-url-error"></span>
-        <button type="submit" aria-label="Создать" className="popup__submit-btn">Создать</button>
       </PopupWithForm>
-      <PopupWithForm title={'Обновить аватар'}
-                     name={'change-avatar'} isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+      <PopupWithForm title='Обновить аватар' name={'change-avatar'}
+                     isOpen={isEditAvatarPopupOpen} btnText={'Сохранить'} onClose={closeAllPopups}>
         <input type="url" required id="avatar-url" placeholder="Ссылка на изображение"
                className="popup__form-item text-input"/>
         <span className="popup__input-error avatar-url-error"></span>
-        <button type="submit" className="popup__submit-btn">Сохранить</button>
       </PopupWithForm>
     </>
   );
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,12 +1,13 @@
-function PopupWithForm({name, title,  isOpen, onClose, children}) {
+function PopupWithForm({name, title,  isOpen, onClose, btnText, children}) {
   return (
     <div className={`popup popup_${name} ${isOpen ? 'popup_opened' : ''}`}>
       <div className="popup__container">
         <h2 className="popup__title">
           {title}
         </h2>
-        <form action="submit" name={`${children}`} noValidate className="popup__form">
+        <form action="submit" noValidate className="popup__form">
           {children}
+          <button type="submit" aria-label={btnText} className="popup__submit-btn">{btnText}</button>
         </form>
         <button type="button" aria-label="Закрыть попап" onClick={onClose} className="popup__closed-btn"></button>
       </div>
